b486c1fe42e0b7c8a22a49eafe2b923b442f6318
Nathaniel Adams <NathanielBAdams@gmail.com>
2020-10-02 15:55:26 -0400

    first commit

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div>hey!</div>;
+  }
+}
+
+export default App;
